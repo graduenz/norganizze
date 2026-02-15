4ecf667037ad067fd58798c6a2289b7c3bab440e
Guilherme Raduenz <graduenz@gmail.com>
2026-02-15 14:38:20 -0300

    chore: fixed some issues revealed by sonar

--- NOrganizze/DateOnlyJsonConverter.cs
+++ NOrganizze/DateOnlyJsonConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 #if NET8_0_OR_GREATER
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -7,29 +8,36 @@ namespace NOrganizze
 {
     internal class DateOnlyJsonConverter : JsonConverter<DateTime>
     {
+        private const string DateFormat = "yyyy-MM-dd";
+
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateTime.Parse(reader.GetString());
+            var dateString = reader.GetString();
+            return DateTime.ParseExact(dateString, DateFormat, CultureInfo.InvariantCulture);
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToString("yyyy-MM-dd"));
+            writer.WriteStringValue(value.ToString(DateFormat, CultureInfo.InvariantCulture));
         }
     }
 
     internal class NullableDateOnlyJsonConverter : JsonConverter<DateTime?>
     {
+        private const string DateFormat = "yyyy-MM-dd";
+
         public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             var stringValue = reader.GetString();
-            return string.IsNullOrEmpty(stringValue) ? (DateTime?)null : DateTime.Parse(stringValue);
+            return string.IsNullOrEmpty(stringValue) 
+                ? (DateTime?)null 
+                : DateTime.ParseExact(stringValue, DateFormat, CultureInfo.InvariantCulture);
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
         {
             if (value.HasValue)
-                writer.WriteStringValue(value.Value.ToString("yyyy-MM-dd"));
+                writer.WriteStringValue(value.Value.ToString(DateFormat, CultureInfo.InvariantCulture));
             else
                 writer.WriteNullValue();
         }
@@ -46,6 +54,7 @@ namespace NOrganizze
         public DateOnlyJsonConverter()
         {
             DateTimeFormat = "yyyy-MM-dd";
+            Culture = CultureInfo.InvariantCulture;
         }
     }
 }
